1d333d2573c8ae2bb9948e44d39532fcea04c53f
Md Somad <mdsomad10@gmail.com>
2024-04-18 21:04:07 +0530

    Part 07 Complete

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,41 +1,25 @@
-import { Controller, Get, Param, Query, Headers } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
-interface videosparams {
-  id: number;
+interface VideoDTO {
   name: string;
+  tag: string;
 }
-interface queryParam {
-  age: number;
-  name: string;
-}
-
 @Controller('users') //* <-- Iska naam hai decorator
 export class Usercontroller {
-  @Get('/videos/:id/:name')
-  getVideos(@Param() params: videosparams) {
-    console.log(params);
-    return 'Success';
+  @Post('/video')
+  addVideo(@Body() requestData: VideoDTO) {
+    console.log(requestData.name, requestData.tag);
+    return {
+      success: true,
+    };
   }
 
-  //* no data type defined params
-  // @Get('/videos/:id')
-  // getVideos(@Param() params:Record<string,any>) {
-  //   console.log(params);
-  //   return 'Success';
-  // }
-
-  //* QueryParam use
-  @Get('/videos2')
-  getVideos2(@Query() query: queryParam) {
-    console.log(query);
-    console.log(query.name);
-    return 'Success';
-  }
-
-  //* headers data get
-  @Get('/headers')
-  getHeaders(@Headers() headers: Record<string, any>) {
-    console.log(headers);
-    return 'Success';
+  //*👇 Specific key extract
+  @Post('/video2')
+  addVideo2(@Body('name') requestData: string) {
+    console.log(requestData);
+    return {
+      success: true,
+    };
   }
 }
